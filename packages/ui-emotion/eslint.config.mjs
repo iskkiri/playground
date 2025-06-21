f23362474a3f42e4b3f5726d6e29565632390b91
iskkiri <yoon_1139@naver.com>
2025-06-21 23:40:08 +0900

    feat: vite + storybook 설정

--- packages/ui-emotion/eslint.config.mjs
+++ packages/ui-emotion/eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import { config } from '@repo/eslint-config/react-internal';
 
 /** @type {import("eslint").Linter.Config} */
@@ -8,4 +11,5 @@ export default [
       'react/no-unknown-property': ['error', { ignore: ['css'] }],
     },
   },
+  ...storybook.configs['flat/recommended'],
 ];
