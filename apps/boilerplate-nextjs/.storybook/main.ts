cddaf31e13561c107c8ce17dbc41f8cc87ebfcec
iskkiri <yoon_1139@naver.com>
2025-06-21 22:23:43 +0900

    feat: 스토리북 설정

--- apps/boilerplate-nextjs/.storybook/main.ts
+++ apps/boilerplate-nextjs/.storybook/main.ts
@@ -1,11 +1,19 @@
+import { join, dirname } from 'path';
 import type { StorybookConfig } from '@storybook/nextjs';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-docs'],
   framework: {
-    name: '@storybook/nextjs',
+    name: getAbsolutePath('@storybook/nextjs'),
     options: {
       nextConfigPath: require.resolve('../next.config.ts'),
     },
--- apps/boilerplate-react/eslint.config.js
+++ apps/boilerplate-react/eslint.config.js
@@ -1,19 +1,17 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import tanstackQuery from '@tanstack/eslint-plugin-query';
-// import storybook from 'eslint-plugin-storybook';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      // ...storybook.configs.recommended,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -42,5 +40,6 @@ export default tseslint.config(
       ],
       '@tanstack/query/exhaustive-deps': 'warn',
     },
-  }
+  },
+  storybook.configs['flat/recommended']
 );
