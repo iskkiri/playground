d72efee9e7f4b980dc61035e32857188d04736b4
iskkiri <yoon_1139@naver.com>
2025-06-21 21:30:02 +0900

    feat: tailwind 설정

--- apps/boilerplate-react/vite.config.ts
+++ apps/boilerplate-react/vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react({
       jsxImportSource: '@emotion/react',
       babel: {
