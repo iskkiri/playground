f0dae911a910a81a65e0faad6753b1a015ea6806
iskkiri <yoon_1139@naver.com>
2025-06-21 18:21:37 +0900

    feat: jotai 설치 및 설정

--- apps/boilerplate-nextjs/next.config.ts
+++ apps/boilerplate-nextjs/next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   compiler: {
     emotion: true,
   },
+  // Learn more here - https://nextjs.org/docs/advanced-features/compiler#module-transpilation
+  // Required for UI css to be transpiled correctly 👇
+  transpilePackages: ['jotai-devtools'],
   turbopack: {
     rules: {
       '*.svg': {
