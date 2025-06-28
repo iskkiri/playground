5b8a24d8af2b13dd568a4cdb63c0740dadd46d1b
iskkiri <yoon_1139@naver.com>
2025-06-28 19:21:59 +0900

    feat: design tokens package - colors

---
+++ packages/design-tokens/src/colors/index.ts
@@ -0,0 +1,67 @@
+import './index.css';
+
+export const colors = {
+  /** #2563eb */
+  primary: 'var(--color-primary, #2563eb)',
+  /** #3b82f6 */
+  primaryHover: 'var(--color-primary-hover, #3b82f6)',
+  /** #ef4444 */
+  danger: 'var(--color-danger, #ef4444)',
+  /** #f87171 */
+  dangerHover: 'var(--color-danger-hover, #f87171)',
+  /** #fdbd47 */
+  warning: 'var(--color-warning, #fdbd47)',
+  /** #14b8a6 */
+  success: 'var(--color-success, #14b856)',
+
+  /** #ffffff */
+  white: 'var(--color-white, #ffffff)',
+  /** #000000 */
+  black: 'var(--color-black, #000000)',
+
+  /** #fafafa */
+  neutral50: 'var(--color-neutral-50, #fafafa)',
+  /** #f5f5f5 */
+  neutral100: 'var(--color-neutral-100, #f5f5f5)',
+  /** #e5e5e5 */
+  neutral200: 'var(--color-neutral-200, #e5e5e5)',
+  /** #d4d4d4 */
+  neutral300: 'var(--color-neutral-300, #d4d4d4)',
+  /** #a3a3a3 */
+  neutral400: 'var(--color-neutral-400, #a3a3a3)',
+  /** #737373 */
+  neutral500: 'var(--color-neutral-500, #737373)',
+  /** #525252 */
+  neutral600: 'var(--color-neutral-600, #525252)',
+  /** #404040 */
+  neutral700: 'var(--color-neutral-700, #404040)',
+  /** #262626 */
+  neutral800: 'var(--color-neutral-800, #262626)',
+  /** #171717 */
+  neutral900: 'var(--color-neutral-900, #171717)',
+  /** #0a0a0a */
+  neutral950: 'var(--color-neutral-950, #0a0a0a)',
+
+  /** #f9fafb */
+  gray50: 'var(--color-gray-50, #f9fafb)',
+  /** #f3f4f6 */
+  gray100: 'var(--color-gray-100, #f3f4f6)',
+  /** #e5e7eb */
+  gray200: 'var(--color-gray-200, #e5e7eb)',
+  /** #d1d5db */
+  gray300: 'var(--color-gray-300, #d1d5db)',
+  /** #9ca3af */
+  gray400: 'var(--color-gray-400, #9ca3af)',
+  /** #6b7280 */
+  gray500: 'var(--color-gray-500, #6b7280)',
+  /** #4b5563 */
+  gray600: 'var(--color-gray-600, #4b5563)',
+  /** #374151 */
+  gray700: 'var(--color-gray-700, #374151)',
+  /** #1f2937 */
+  gray800: 'var(--color-gray-800, #1f2937)',
+  /** #111827 */
+  gray900: 'var(--color-gray-900, #111827)',
+  /** #030712 */
+  gray950: 'var(--color-gray-950, #030712)',
+} as const;
