4904ef8c84e1fbcebf24d94ea32a77e9632f9558
iskkiri <yoon_1139@naver.com>
2025-06-29 02:46:01 +0900

    feat: eslint & prettier 설정
    - @repo/eslint-config 적용
    - @repo/prettier-config 적용
    - prettier-plugin-tailwindcss 설치
    - globals.css -> styles/globals.css 위치 변경

---
+++ apps/web-examples/.prettierrc.mjs
@@ -0,0 +1,14 @@
+import { config as baseConfig } from '@repo/prettier-config/base';
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  ...baseConfig,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/styles/  globals.css',
+  tailwindFunctions: ['clsx', 'cn', 'cva'],
+};
+
+export default config;
