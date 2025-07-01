67da925709494e30d6b69fb9a684fa5b4b0cec22
iskkiri <yoon_1139@naver.com>
2025-07-02 02:43:27 +0900

    feat: mocks

--- apps/web-examples/src/_mocks/server.ts
+++ apps/web-examples/src/_mocks/server.ts
@@ -2,17 +2,17 @@ import { setupServer } from 'msw/node';
 import { userHandlers } from '@/_features/user/mocks/handlers/userHandlers';
 import { fileHandlers } from '@/_features/file/mocks/handlers/fileHandler';
 import { imageHandlers } from '@/_features/image/mocks/handlers/imageHandlers';
-// import { noticeHandlers } from '../app/(admin)/_features/notice/mocks/handlers/noticeHandlers';
-// import { bannerHandlers } from '@/app/(admin)/_features/banner/mocks/handlers/bannerHandlers';
-// import { adminPopupHandlers as adminPopupHandlers } from '@/app/(admin)/_features/popup/mocks/handlers/popupHandlers';
+import { noticeHandlers } from '../app/(admin)/_features/notice/mocks/handlers/noticeHandlers';
+import { bannerHandlers } from '@/app/(admin)/_features/banner/mocks/handlers/bannerHandlers';
+import { adminPopupHandlers as adminPopupHandlers } from '@/app/(admin)/_features/popup/mocks/handlers/popupHandlers';
 // import { popUpHandlers as clientPopUpHandlers } from '@/app/(client)/_features/popUp/mocks/handlers/popUpHandlers';
 
 export const server = setupServer(
   ...userHandlers,
   ...fileHandlers,
-  ...imageHandlers
-  // ...noticeHandlers,
-  // ...bannerHandlers,
-  // ...adminPopupHandlers
+  ...imageHandlers,
+  ...noticeHandlers,
+  ...bannerHandlers,
+  ...adminPopupHandlers
   // ...clientPopUpHandlers
 );
