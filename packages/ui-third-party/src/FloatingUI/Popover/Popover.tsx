2e7eae1458b8920d89cfb960c4825555d3888b59
iskkiri <yoon_1139@naver.com>
2025-09-05 21:46:05 +0900

    refactor: Popover prop 구조 개선 및 상태 관리 로직 개선

--- packages/ui-third-party/src/FloatingUI/Popover/Popover.tsx
+++ packages/ui-third-party/src/FloatingUI/Popover/Popover.tsx
@@ -19,6 +19,22 @@ import PopoverContent from './PopoverContent';
 
 interface PopoverProps extends PopoverOptions {
   children: React.ReactNode;
+  /**
+   * controlled일 때(isOpen이 외부에서 주어졌을 경우) 내부 인터랙션은 기본적으로 비활성화
+   * 만약 외부에서 상태값을 넘겨주고, 내부 인터랙션을 사용하고자 할 경우 isInteractionEnabled을 true로 설정
+   * 스토리북 Controlled 예제 참고
+   * */
+  isInteractionEnabled?: boolean;
+  /**
+   * PopoverContent 내부의 요소를 클릭했을 때 팝오버를 닫을지 여부를 결정합니다.
+   * 기본값은 false입니다.
+   */
+  dismissOnContentClick?: boolean;
+  /**
+   * PopoverContent 내부의 요소에 포커스를 주지 않을지 여부를 결정합니다.
+   * 기본값은 false입니다.
+   */
+  isFocusDisabled?: boolean;
 }
 
 /**
@@ -26,10 +42,16 @@ interface PopoverProps extends PopoverOptions {
  * @reference https://codesandbox.io/p/sandbox/distracted-swirles-jo1pvu?file=%2Fsrc%2FPopover.tsx%3A34%2C3-82%2C5
  */
 export default function Popover({ children, ...options }: PopoverProps) {
+  const {
+    isOpen: controlledOpen,
+    onOpenChange: setControlledOpen,
+    isInteractionEnabled,
+    ...restOptions
+  } = options;
   const [uncontrolledOpen, setUncontrolledOpen] = useState(options.initialOpen ?? false);
 
-  const isOpen = options.isOpen ?? uncontrolledOpen;
-  const setIsOpen = options.onOpenChange ?? setUncontrolledOpen;
+  const isOpen = controlledOpen ?? uncontrolledOpen;
+  const setIsOpen = setControlledOpen ?? setUncontrolledOpen;
 
   const data = useFloating({
     placement: options.placement,
@@ -43,8 +65,7 @@ export default function Popover({ children, ...options }: PopoverProps) {
 
   // 참조 요소를 클릭했을 때 popover를 열거나 닫는 기능을 추가
   const click = useClick(context, {
-    enabled:
-      options.isOpen === undefined || options.isOpen === null || options.isInteractionEnabled,
+    enabled: controlledOpen === undefined || controlledOpen === null || isInteractionEnabled,
   });
   // 사용자가 Esc 키를 누를 때 popover를 닫는 기능을 추가
   const dismiss = useDismiss(context);
@@ -57,11 +78,11 @@ export default function Popover({ children, ...options }: PopoverProps) {
     () => ({
       isOpen,
       setIsOpen,
-      dismissOnContentClick: options.dismissOnContentClick,
+      ...restOptions,
       ...interactions,
       ...data,
     }),
-    [isOpen, setIsOpen, options.dismissOnContentClick, interactions, data]
+    [isOpen, setIsOpen, restOptions, interactions, data]
   );
 
   return <PopoverContext.Provider value={values}>{children}</PopoverContext.Provider>;
--- packages/ui-third-party/src/FloatingUI/Popover/types/popover.types.ts
+++ packages/ui-third-party/src/FloatingUI/Popover/types/popover.types.ts
@@ -6,15 +6,4 @@ export interface PopoverOptions {
   isOpen?: boolean;
   onOpenChange?(open: boolean, event?: Event, reason?: OpenChangeReason): void;
   offsetOptions?: OffsetOptions;
-  /**
-   * controlled일 때(isOpen이 외부에서 주어졌을 경우) 내부 인터랙션은 기본적으로 비활성화
-   * 만약 외부에서 상태값을 넘겨주고, 내부 인터랙션을 사용하고자 할 경우 isInteractionEnabled을 true로 설정
-   * 스토리북 Controlled 예제 참고
-   * */
-  isInteractionEnabled?: boolean;
-  /**
-   * PopoverContent 내부의 요소를 클릭했을 때 팝오버를 닫을지 여부를 결정합니다.
-   * 기본값은 false입니다.
-   */
-  dismissOnContentClick?: boolean;
 }
